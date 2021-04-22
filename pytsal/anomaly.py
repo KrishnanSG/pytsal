2fc90f3453ef153e12330fbee8a1dc7b32088019
KrishnanSG <krishsg525@gmail.com>
2021-04-22 12:15:43 +0530

    Update brutlag algorithm params

--- pytsal/anomaly.py
+++ pytsal/anomaly.py
@@ -8,31 +8,32 @@ from pytsal.internal.utils.helpers import get_logger
 LOG = get_logger(__name__)
 
 
-def brutlag_algorithm(ts: TimeSeries, model: HoltWintersResults, tolerance: int = 0.5):
+def brutlag_algorithm(ts: TimeSeries, model: HoltWintersResults, tolerance: int = 0.5, period=12, sf=2,
+                      gamma=0.3684211):
     LOG.info('Performing anomaly detection ...')
 
-    PERIOD = 12  # The given time series has seasonal_period=12
-    GAMMA = 0.3684211  # the seasonality component
-    SF = 2  # brutlag scaling factor for the confidence bands.
-    UB = []  # upper bound or upper confidence band
-    LB = []  # lower bound or lower confidence band
+    # period - seasonal_period of the given time series
+    # gamma - the seasonality component (0.2 -0.5)
+    # sf - brutlag scaling factor for the confidence bands. (2-3)
+    ub = []  # upper bound or upper confidence band
+    lb = []  # lower bound or lower confidence band
     prediction = model.predict(ts.start, ts.end)
 
     difference_array = []
     dt = []
     difference_table = {
-        "actual": ts.data, "predicted": prediction, "difference": difference_array, "UB": UB, "LB": LB}
+        "actual": ts.data, "predicted": prediction, "difference": difference_array, "UB": ub, "LB": lb}
 
     """Calculation of confidence bands using brutlag algorithm"""
     for i in range(len(prediction)):
         diff = ts.data[i] - prediction[i]
-        if i < PERIOD:
-            dt.append(GAMMA * abs(diff))
+        if i < period:
+            dt.append(gamma * abs(diff))
         else:
-            dt.append(GAMMA * abs(diff) + (1 - GAMMA) * dt[i - PERIOD])
+            dt.append(gamma * abs(diff) + (1 - gamma) * dt[i - period])
         difference_array.append(diff)
-        UB.append(prediction[i] + SF * dt[i])
-        LB.append(prediction[i] - SF * dt[i])
+        ub.append(prediction[i] + sf * dt[i])
+        lb.append(prediction[i] - sf * dt[i])
 
     print("\nDifference between actual and predicted\n")
 
@@ -47,7 +48,7 @@ def brutlag_algorithm(ts: TimeSeries, model: HoltWintersResults, tolerance: int 
     actual = []
 
     for i in range(len(ts.data.index)):
-        if (UB[i] <= ts.data[i] or LB[i] >= ts.data[i]) and i > PERIOD and abs(ts.data[i] - prediction[i]) > tolerance:
+        if (ub[i] <= ts.data[i] or lb[i] >= ts.data[i]) and i > period and abs(ts.data[i] - prediction[i]) > tolerance:
             anomaly_date.append(ts.data.index[i])
             anomaly.append(ts.data[i])
             actual.append(prediction[i])
@@ -72,8 +73,8 @@ def brutlag_algorithm(ts: TimeSeries, model: HoltWintersResults, tolerance: int 
     plt.plot(anomaly.index, anomaly[['observed']], 'o', color='red')
 
     # Plotting brutlag confidence bands
-    plt.plot(ts.data.index, UB, linestyle='--', color='grey')
-    plt.plot(ts.data.index, LB, linestyle='--', color='grey')
+    plt.plot(ts.data.index, ub, linestyle='--', color='grey')
+    plt.plot(ts.data.index, lb, linestyle='--', color='grey')
 
     # Formatting the graph
     plt.legend(['Normal', 'Anomaly', 'Upper Bound', 'Lower Bound'])
